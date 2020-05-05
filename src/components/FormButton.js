9ab9d63371875de2468d8b00df57e78195738dfb
Balogun Ridwan Babatunde Ridbay <26006774+ridbay@users.noreply.github.com>
2020-05-05 13:00:19 +0100

    Add Login Screen

--- src/components/FormButton.js
+++ src/components/FormButton.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, Dimensions, Text } from "react-native";
+import { StyleSheet, Dimensions } from "react-native";
 
 import { Button } from "react-native-paper";
 
